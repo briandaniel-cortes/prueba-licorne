7db9745b20cd7cf7333ad1a348293bd5eb02a90f
TESMAN117 <67400857+TESMAN117@users.noreply.github.com>
2021-10-13 17:11:19 -0500

    calculadora

---
+++ Calculadora/index.php
@@ -0,0 +1,12 @@
+
+<?php
+
+for ($i=1; $i <=5; $i++) { 
+    for ($e=1; $e <=5; $e++) { 
+        $calculadora=$e+$i;
+        echo $i." + ".$e."  =".$calculadora."<br/>";
+       
+    }
+    
+}
+?>
