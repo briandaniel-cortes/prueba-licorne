d8ccb5abdfb0a10016a0512e802cf106eeec9b85
TESMAN117 <67400857+TESMAN117@users.noreply.github.com>
2021-10-13 16:14:01 -0500

    Base de datos

---
+++ Basededatos.sql
@@ -0,0 +1,54 @@
+/*
+SQLyog Ultimate v11.11 (64 bit)
+MySQL - 5.5.5-10.1.37-MariaDB : Database - prueba
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`prueba` /*!40100 DEFAULT CHARACTER SET latin1 */;
+
+USE `prueba`;
+
+/*Table structure for table `compra` */
+
+DROP TABLE IF EXISTS `compra`;
+
+CREATE TABLE `compra` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `Compra` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `Compra` (`Compra`),
+  CONSTRAINT `compra_ibfk_1` FOREIGN KEY (`Compra`) REFERENCES `productos` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=latin1;
+
+/*Data for the table `compra` */
+
+insert  into `compra`(`id`,`Compra`) values (1,1),(2,1),(3,1),(4,1),(5,1),(6,2),(7,4);
+
+/*Table structure for table `productos` */
+
+DROP TABLE IF EXISTS `productos`;
+
+CREATE TABLE `productos` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre_producto` varchar(60) NOT NULL,
+  `imagen` varchar(100) NOT NULL,
+  `precio` float NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+
+/*Data for the table `productos` */
+
+insert  into `productos`(`id`,`nombre_producto`,`imagen`,`precio`) values (1,'Xiaomi 9','xiaomi1.jpg',10000),(2,'Xiaomi 6','xiaomi2.jpg',10000),(3,'Xiaomi 5','xiaomi3.jpg',5000),(4,'Xiaomi 4','xiaomi4.jpg',10000);
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
